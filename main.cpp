5b2501f865978894a142b6321b69e9a34a8743ac
khromenokroman <roma55592@yandex.ru>
2024-11-17 22:09:59 +0200

    Добавлена обработка ситуации когда размер файла не кратен буферу Добавлен пример файла

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include <liburing.h>
 
 #include <array>
+#include <cassert>
 #include <cstring>
 #include <iostream>
 #include <istream>
 
 //@todo: надо нормально выходить закрывать предыдущие ресурсы
-static constexpr size_t MAX_BUFFER_SIZE_READ = 1024u * 1024u * 2014u * 1024u;
-static constexpr size_t MAX_BUFFER_SIZE_WRITE = 100;
-static constexpr uint8_t MAX_TASKS_IO = 20;
+static constexpr size_t MAX_BUFFER_SIZE_READ = 46;
+static constexpr size_t MAX_BUFFER_SIZE_WRITE = 5;
+static constexpr uint8_t MAX_TASKS_IO = 5;
 int main() {
     //@todo: по хорошему чтение надо тоже через uring сделать
-    char *buffer_read = reinterpret_cast<char *>(malloc(MAX_BUFFER_SIZE_READ));
+    char buffer_read[MAX_BUFFER_SIZE_READ];
     int fd_file_read = open("test_ur.txt", O_RDONLY, 660);
     if (fd_file_read < 0) {
         perror(strerror(errno));
@@ -34,7 +35,6 @@ int main() {
         auto err = errno;
         close(fd_file_write);
         close(fd_file_read);
-        free(buffer_read);
         perror(strerror(err));
         return -1;
     } else if (bytes == 0) {
@@ -52,7 +52,6 @@ int main() {
         auto err = errno;
         close(fd_file_write);
         close(fd_file_read);
-        free(buffer_read);
         perror(strerror(err));
         return -1;
     }
@@ -61,13 +60,29 @@ int main() {
     uint64_t confirmed_write = 0;
     uint64_t tasks = 1;
     while (true) {
-        if (tasks == 1) {
+        if (tasks == 1 && curr_bytes_write != size_file) {
             while (true) {
                 struct io_uring_sqe *sqe = io_uring_get_sqe(&uring_instance);
-                io_uring_prep_write(sqe, fd_file_write, buffer_read + curr_bytes_write, MAX_BUFFER_SIZE_WRITE, curr_bytes_write);
-                curr_bytes_write += MAX_BUFFER_SIZE_WRITE;
+                if (!sqe) {
+                    auto err = errno;
+                    close(fd_file_write);
+                    close(fd_file_read);
+                    perror(strerror(err));
+                    return -1;
+                }
+                assert(curr_bytes_write <= MAX_BUFFER_SIZE_READ);
+                auto dif = size_file - curr_bytes_write;
+                if (dif >= MAX_BUFFER_SIZE_WRITE) {
+                    io_uring_prep_write(sqe, fd_file_write, buffer_read + curr_bytes_write, MAX_BUFFER_SIZE_WRITE, curr_bytes_write);
+                    curr_bytes_write += MAX_BUFFER_SIZE_WRITE;
+                } else {
+                    std::cout << "find tail = " << dif << std::endl;
+                    io_uring_prep_write(sqe, fd_file_write, buffer_read + curr_bytes_write, dif, curr_bytes_write);
+                    curr_bytes_write += dif;
+                }
                 sqe->user_data = tasks;
-                if (tasks == MAX_TASKS_IO - 1 || curr_bytes_write == size_file) {
+                std::cout << "load: " << tasks << " tasks" << std::endl;
+                if (tasks == MAX_TASKS_IO || curr_bytes_write == size_file) {
                     break;
                 }
                 tasks++;
@@ -76,7 +91,6 @@ int main() {
                 auto err = errno;
                 close(fd_file_write);
                 close(fd_file_read);
-                free(buffer_read);
                 perror(strerror(err));
                 return -1;
             } else {
@@ -85,20 +99,18 @@ int main() {
         } else {
             while (tasks != 0) {
                 struct io_uring_cqe *cqes = nullptr;
-                if (uint64_t completed_task = io_uring_peek_batch_cqe(&uring_instance, &cqes, MAX_TASKS_IO); completed_task > 0) {
-                    std::cout << "=== found " << completed_task << " completed task" << std::endl;
-                    for (uint64_t i = 0; i < completed_task; i++) {
-                        if (cqes[i].res > 0) {
-                            std::cout << "\ttask: " << cqes[i].user_data << " write bytes: " << bytes << std::endl;
-                        } else {
-                            std::cout << "error res" << bytes << std::endl;
-                        }
-                        io_uring_cqe_seen(&uring_instance, &cqes[0]);
-                        confirmed_write += MAX_BUFFER_SIZE_WRITE;
+                if (uint64_t completed_task = io_uring_wait_cqe(&uring_instance, &cqes); completed_task == 0) {
+                    std::cout << "=== found completed task" << std::endl;
+                    if (cqes[0].res > 0) {
+                        confirmed_write += cqes[0].res;
                         tasks--;
+                        std::cout << "\ttask: " << cqes[0].user_data << " total need write bytes: " << size_file
+                                  << " write bytes: " << confirmed_write << std::endl;
+                        io_uring_cqe_seen(&uring_instance, cqes);
                     }
                 }
             }
+            tasks = 1;
         }
         if (confirmed_write == size_file) {
             break;
@@ -107,6 +119,5 @@ int main() {
     io_uring_queue_exit(&uring_instance);
     close(fd_file_write);
     close(fd_file_read);
-    free(buffer_read);
     return 0;
 }
